d33facc07be61210d11bf488bc5e11ff95e08f55
Raymond Ativie <raymondativie@gmail.com>
2018-07-13 20:14:25 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<nav class="navbar navbar-light bg-light justify-content-center shadow-sm">
+			<a class="navbar-brand" href="#">Codebase Weather</a>
+		</nav>
+		<router-view/>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomePage from '@/components/pages/HomePage';
+import SearchPage from '@/components/pages/SearchPage';
+import DetailPage from '@/components/pages/DetailPage';
+
+Vue.use(Router);
+
+export default new Router({
+	routes: [
+		{
+			path: '/',
+			name: 'HomePage',
+			component: HomePage,
+		},
+		{
+			path: '/search/:keyword',
+			name: 'SearchPage',
+			component: SearchPage,
+		},
+		{
+			path: '/weather/:woeid',
+			name: 'DetailPage',
+			component: DetailPage,
+		},
+	]
+});
